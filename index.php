e166520e0802aaa4c72f079ec023ce559a10c24f
Khemraj <khemraj@rwit.io>
2024-12-18 15:23:59 +0530

    Change root file name

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-  include('layout/header.php');
-  include('components/inroduction.php');
-  include('components/services.php');
-  include('components/about.php');
-  include('components/work.php');
-  include('layout/footer.php');
-?>
